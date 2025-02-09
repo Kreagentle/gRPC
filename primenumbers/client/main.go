8fd2b43e4b71f46f9810099f001d169902e0ff23
Kreagentle <ponwrt@gmail.com>
2025-02-09 23:40:57 +0000

    Add client for prime numbers task

---
+++ primenumbers/client/main.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"log"
+
+	pb "github.com/Kreagentle/gRPC/primenumbers/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const address = "0.0.0.0:8085"
+
+func main() {
+	connection, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v\n", err)
+	}
+	defer func(connection *grpc.ClientConn) {
+		err = connection.Close()
+		if err != nil {
+			log.Printf("Failed to close connection: %v\n", err)
+		}
+	}(connection)
+
+	log.Printf("Connected to: %s\n", address)
+
+	_ = pb.NewCalculatorClient(connection)
+}
