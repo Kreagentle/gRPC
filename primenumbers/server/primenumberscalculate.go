aa1ebcfb0fd267bad8687be03c73cd18cb4da3ac
Kreagentle <ponwrt@gmail.com>
2025-02-13 22:52:12 +0000

    Redo the server

--- primenumbers/server/primenumberscalculate.go
+++ primenumbers/server/primenumberscalculate.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"log"
 
 	pb "github.com/Kreagentle/gRPC/primenumbers/proto"
 )
 
-func (s *Server) Calculate(c context.Context, in *pb.PrimeRequest) (*pb.PrimeResponse, error) {
-	log.Printf("Calculation that happend from the server side %v\n", in)
-	return &pb.PrimeResponse{
-		Number3: in.Number1 + in.Number2,
-	}, nil
+func (s *Server) CalculatePrimeNumbers(in *pb.PrimeRequest, stream pb.Calculator_CalculateServer) error {
+	log.Printf("Function CalculatePrimeNumbers is invoked %v\n\n", in)
+	for i := range []int{1, 2, 3, 4, 5} {
+		data := fmt.Sprintf("Invokes %d the data %d", in.Number1, i)
+
+		err := stream.Send(&pb.PrimeResponse{
+			Number2: data,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
