0a7c850b3a8108e08967790dd49485df7f4487fc
Kreagentle <ponwrt@gmail.com>
2025-02-24 23:37:58 +0000

    Add average

---
+++ average/server/avgcalculate.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/Kreagentle/gRPC/average/proto"
+)
+
+func (s *Server) AvgCalculate(stream pb.Calculator_CalculateServer) error {
+	log.Printf("Function AvgCalculate is invoked")
+	var sum int32
+	var count int
+
+	for {
+		rec, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.AvgResponse{Number2: float64(sum) / float64(count)})
+		}
+
+		if err != nil {
+			log.Fatalf("cant receive the message in the server side %v", err)
+		}
+
+		sum += rec.Number
+		count++
+	}
+	return nil
+}
